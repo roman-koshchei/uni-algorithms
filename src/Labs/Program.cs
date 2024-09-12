cc9dec1618d1bd0e3838dc3cbdf807dbad7132dd
Roman Koshchei <romankoshchei@gmail.com>
2024-09-12 16:06:17 +0300

    init

---
+++ src/Labs/Program.cs
@@ -0,0 +1,21 @@
+﻿var maxPrice = MaxPriceForWeight([new(5, 4), new(7, 5), new(4, 3), new(9, 7), new(8, 6)], 16);
+Console.WriteLine(maxPrice);
+
+int MaxPriceForWeight(IEnumerable<Item> items, int maxWeight)
+{
+    var maxPrice = 0;
+    foreach (var item in items)
+    {
+        var newMaxWeight = maxWeight - item.Weight;
+        if (newMaxWeight < 0) continue;
+
+        var newCurrentPrice = item.Price + MaxPriceForWeight(items.Where(x => x != item), newMaxWeight);
+        if (newCurrentPrice > maxPrice)
+        {
+            maxPrice = newCurrentPrice;
+        }
+    }
+    return maxPrice;
+}
+
+record Item(int Price, int Weight);
